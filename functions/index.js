98a1088ef621eef8f52d62201a90a7ddfc23c79a
tejastamkar <tejas.tamkar@gmail.com>
2023-04-05 12:09:28 +0530

    added application rejection msg

--- functions/index.js
+++ functions/index.js
@@ -43,3 +43,113 @@ export const newschemeAlert = functions
 
     return Promise.resolve();
   });
+
+export const statusNotification = functions
+  .region("asia-south1")
+  .firestore.document("Users/{userId}")
+  .onUpdate(async (snapshot, context) => {
+    var oldUserDetails = snapshot.before.data();
+    var userDetails = snapshot.after.data();
+    var userId = context.params.userId;
+
+    var checkNotifcationCenter = await db
+      .doc(`NotificationCenter/${userId}`)
+      .get()
+      .then((value) => value.exists);
+    if (checkNotifcationCenter) {
+      var getNotificationDetails = (checkNotifcationCenter = await db
+        .doc(`NotificationCenter/${userId}`)
+        .get()
+        .then((value) => value.data()));
+    }
+
+    if (
+      !userDetails["registeration"] &&
+      oldUserDetails["msg"] != userDetails["msg"] &&
+      userDetails["msg"] != ""
+    ) {
+      const payload = {
+        token: userDetails["token"].toString(),
+        notification: {
+          title: "Scheme Suvidha",
+          body: `Your registeration is rejected due to, ${userDetails["msg"]}.`,
+        },
+        data: {
+          body: `You have completed the registeration , Your Application will be verify soon.`,
+        },
+      };
+      admin
+        .messaging()
+        .send(payload)
+        .then((response) => {
+          // Response is a message ID string.
+          console.log("its working");
+          console.log("Successfully sent message:", response);
+        })
+        .catch((error) => {
+          console.log("error:", error.code);
+        });
+    }
+    if (!checkNotifcationCenter && userDetails["registeration"]) {
+      await db.doc(`NotificationCenter/${userId}`).create({
+        name: userDetails["name"],
+        registeration: true,
+        verification: false,
+        sendVerificationNotification: false,
+      });
+      const payload = {
+        token: userDetails["token"].toString(),
+        notification: {
+          title: "Scheme Suvidha",
+          body: `You have completed the registeration , Your Application will be verify soon.`,
+        },
+        data: {
+          body: `You have completed the registeration , Your Application will be verify soon.`,
+        },
+      };
+      admin
+        .messaging()
+        .send(payload)
+        .then((response) => {
+          // Response is a message ID string.
+          console.log("its working");
+          console.log("Successfully sent message:", response);
+        })
+        .catch((error) => {
+          console.log("error:", error.code);
+        });
+    }
+
+    if (
+      checkNotifcationCenter &&
+      !getNotificationDetails["sendVerificationNotification"]
+    ) {
+      await db.doc(`NotificationCenter/${userId}`).update({
+        verification: true,
+        sendVerificationNotification: true,
+      });
+      const payload = {
+        token: userDetails["token"].toString(),
+        notification: {
+          title: "Scheme Suvidha",
+          body: `Your Application have been verified , Now you can apply for any schemes`,
+        },
+        data: {
+          body: `Your Application have been verified , Now you can apply for any schemes`,
+        },
+      };
+      admin
+        .messaging()
+        .send(payload)
+        .then((response) => {
+          // Response is a message ID string.
+          console.log("its working");
+          console.log("Successfully sent message:", response);
+        })
+        .catch((error) => {
+          console.log("error:", error.code);
+        });
+    }
+
+    return Promise.resolve();
+  });
--- functions/putdata.js
+++ functions/putdata.js
@@ -1,5 +1,5 @@
 // import { initializeApp, credential as _credential } from "firebase-admin";
-import { data } from "./data.js";
+// import { data } from "./data.js";
 import admin from "firebase-admin";
 import serviceAccount from "./serviceAccountKey.json" assert { type: "json" };
 
@@ -12,6 +12,20 @@ async function uploadData() {
     await db.collection("Schemes").add(data);
   });
 }
+async function updateschemeID() {
+  var schemes = [];
 
-uploadData();
-
+  await db
+    .collection("Schemes")
+    .get()
+    .then((value) => {
+      value.docs.forEach((doc) => {
+        schemes.push(doc.id);
+      });
+    });
+  schemes.forEach(async (id) => {
+    await db.collection("Schemes").doc(id).update({ schemeId: id });
+  });
+}
+updateschemeID();
+// uploadData();
