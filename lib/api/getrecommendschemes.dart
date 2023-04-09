9c23ec8b14ab11ec8c2fb93513d7371265123341
tejastamkar <tejas.tamkar@gmail.com>
2023-04-10 00:22:37 +0530

    added disable type to recommed system

--- lib/api/getrecommendschemes.dart
+++ lib/api/getrecommendschemes.dart
@@ -1,10 +1,24 @@
 import 'dart:math';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:scheme/data/userdata.dart';
 
 Future<List> getrecommendApi() async {
   final random = Random();
   List schemedata = [], recommendList = [];
+  if (userDetail != null) {
+    await FirebaseFirestore.instance
+        .collection("Schemes")
+        .where("disabilityType",
+            isEqualTo: userDetail!.disabilitytype.toString())
+        .get()
+        .then((QuerySnapshot querysnapshot) async {
+      for (var doc in querysnapshot.docs) {
+        schemedata.add(doc.data());
+      }
+      // schemedata.removeWhere((element) => element["validity"] != "Every Year");
+    });
+  }
   await FirebaseFirestore.instance
       .collection("Schemes")
       .where("disabilityType", isEqualTo: "All Disability")
@@ -13,11 +27,12 @@ Future<List> getrecommendApi() async {
     for (var doc in querysnapshot.docs) {
       schemedata.add(doc.data());
     }
-    schemedata.removeWhere((element) => element["validity"] != "Every Year");
-    for (int i = 0; i < 3; i++) {
-      recommendList.add(schemedata[random.nextInt(schemedata.length)]);
-    }
-    return recommendList;
+    // schemedata.removeWhere((element) => element["validity"] != "Every Year");
   });
+
+  for (int i = 0; i < 3; i++) {
+    recommendList.add(schemedata[random.nextInt(schemedata.length)]);
+  }
+
   return recommendList;
 }
